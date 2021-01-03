7bd652822c9a6c7d81f234638dc964a4952b5c01
Tobias Kraus (Lenovo) <contact@tobias-kraus.com>
2021-01-03 14:35:41 +0100

    add await-to-js & refactor Promise chain

--- src/api/post-upload.ts
+++ src/api/post-upload.ts
@@ -1,4 +1,5 @@
 import formidable from "express-formidable";
+import { to } from "await-to-js";
 
 import { app } from "../app";
 import { isAuthorized } from "../utils/auth";
@@ -6,7 +7,7 @@ import { bucket } from "../storage";
 import { propertiesCollection } from "../database";
 import { Upload } from "../models/upload";
 
-app.post("/upload", formidable(), isAuthorized, (req, res) => {
+app.post("/upload", formidable(), isAuthorized, async (req, res) => {
     const files = req.files;
     const propertyId = req.fields?.propertyId;
     const filesArray = files ? Object.values(files) : [];
@@ -24,33 +25,46 @@ app.post("/upload", formidable(), isAuthorized, (req, res) => {
         return;
     }
     const pathInBucket = `properties/${propertyId}/uploads/${filesArray[0].name}`;
-    bucket
-        .upload(filesArray[0].path, {
+
+    const [errUpload, uploadResponse] = await to(
+        bucket.upload(filesArray[0].path, {
             gzip: true,
             destination: pathInBucket,
             metadata: {
                 cacheControl: "public, max-age=31536000",
             },
         })
-        .then(([file]) => {
-            const upload: Upload = {
-                name: filesArray[0].name,
-                url: `https://storage.googleapis.com/${bucket.name}/${file.name}`,
-            };
-            return propertiesCollection.updateMany(
-                { scout_id: propertyId },
-                { $push: { uploads: upload } }
-            );
-        })
-        .then((updateResult) => {
-            res.status(201).send({
-                message: `uploaded to bucket '${bucket.name}': '${pathInBucket}' and modified in Database ${updateResult.modifiedCount} Document(s).`,
-            });
-        })
-        .catch((err) => {
-            res.status(500).send({
-                message: `couldn't upload to bucket "${bucket.name}": ${pathInBucket}`,
-                err,
-            });
+    );
+    if (errUpload || !uploadResponse) {
+        res.status(500).send({
+            message: `couldn't upload to bucket "${bucket.name}": ${pathInBucket}`,
+            errUpload,
         });
+        return;
+    }
+
+    const file = uploadResponse[0];
+    const upload: Upload = {
+        name: filesArray[0].name,
+        url: `https://storage.googleapis.com/${bucket.name}/${file.name}`,
+    };
+
+    const [errUpdateDb, updateDbResponse] = await to(
+        propertiesCollection.updateMany({ scout_id: propertyId }, { $push: { uploads: upload } })
+    );
+    if (errUpdateDb || !updateDbResponse) {
+        res.status(500).send({
+            message: `couldn't update DB. But file was saved to bucket`,
+            errUpdateDb,
+        });
+        return;
+    }
+
+    res.status(201).send({
+        message: `uploaded to bucket '${bucket.name}': '${pathInBucket}' and modified in Database ${updateDbResponse.modifiedCount} Document(s).`,
+    });
 });
+
+async function uploadFileAndUpdateDb(propertyId: string, file: File) {
+    return new Promise((resolve, reject) => {});
+}
